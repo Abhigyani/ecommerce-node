0eaf061baa2a666d01e49e4615db3f6741375016
Abhigyani Anand <abhigyani.97@gmail.com>
2021-11-29 14:02:37 +0530

    Updated cart model logic

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,43 +1,32 @@
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path')
 
-const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json')
+const rootDir = require('../utils/path');
 
-class Cart {
-    /**
-     *  // Fetch the previous cart.
-        // Analyse the cart => Find the existing product.
-        // Add new product/ increase quantity.
-     * @param {*} id 
-     */
-    static addProduct(id, productPrice) {
-        // Fetching the previous cart.
+const p = path.join(rootDir, 'data', 'cart.json');
+class Cart{
+    static addProduct(productId, productPrice) {
         fs.readFile(p, (err, fileContent) => {
-            let cart = {products: [], totalPrice: 0};
+            let cart = {products:[], totalPrice: 0}
             if(!err) {
                 cart = JSON.parse(fileContent);
             }
-
-            //Analyse the cart and find the existing product.
-            const existingProductIndex = cart.products.findIndex(product => product.id === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-            if(existingProduct) {
-                updatedProduct = { ...existingProduct };
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
+            let productIndex = cart.products.findIndex((product) => product.id == productId);
+            if(productIndex === -1) {
+                let incomingProduct = {id: productId, qty: 1}
+                cart.products = [...cart.products, incomingProduct];
             } else {
-                updatedProduct = { id: id, qty: 1 };
-                cart.products = [...cart.products, updatedProduct]
+                cart.products[productIndex]['qty'] = cart.products[productIndex]['qty'] + 1;
             }
             cart.totalPrice = cart.totalPrice + parseInt(productPrice);
 
             fs.writeFile(p, JSON.stringify(cart), (err) => {
-                console.log(err);
-            })
+                if(err) {
+                    console.log('Saving to cart failed!');
+                }
+            });
         });
     }
 }
 
-module.exports = Cart;
+module.exports = Cart
